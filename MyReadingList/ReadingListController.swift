9fd1638a8c2dd5540b5944208c166338c7ff726d
LaKyah Tyner <lakyahtyner@gmail.com>
2019-10-28 12:06:29 -0500

    Implemented delete book

--- MyReadingList/ReadingListController.swift
+++ MyReadingList/ReadingListController.swift
@@ -57,13 +57,13 @@ class ReadingListController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    /*
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        db.delete(indexPath)
-        //save()
+        dbDelagate.delete(books[indexPath.row].title)
+        books.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .automatic)
     }
-     */
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return books.count
